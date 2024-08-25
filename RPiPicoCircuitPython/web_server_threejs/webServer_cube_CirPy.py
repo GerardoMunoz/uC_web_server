c36906988cd1a41ccafffa9f039f5f0f9db1d12b
Gerardo Munoz <36160037+GerardoMunoz@users.noreply.github.com>
2024-08-24 20:42:50 -0500

    Add files via upload

---
+++ RPiPicoCircuitPython/web_server_threejs/webServer_cube_CirPy.py
@@ -0,0 +1,47 @@
+import socketpool
+import wifi
+
+URL_other='https://raw.githubusercontent.com'
+
+wifi.radio.connect("Ejemplo","12345678")
+pool=socketpool.SocketPool(wifi.radio)
+
+print("wifi.radio",wifi.radio.hostname, wifi.radio.ipv4_address)
+s = pool.socket()
+s.setsockopt(pool.SOL_SOCKET, pool.SO_REUSEADDR, 1)
+s.bind(('', 80))
+s.listen(5)
+response = """
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="utf-8">
+        <title>My first cube in three.js</title>
+        <style>
+            body { margin: 0; }
+        </style>
+        <script src="https://cdn.jsdelivr.net/npm/three@0.126.1/build/three.min.js"></script>
+    </head>
+    <body>
+        <div id="cube"></div>
+        <h1>Cube</h1>
+        <script src="https://gerardomunoz.github.io/uC_web_server/RPiPicoCircuitPython/web_server_threejs/cube_three_js.js"></script>
+    </body>
+</html>
+"""
+
+
+while True:
+  conn, addr = s.accept()
+  print('Got a connection from %s' % str(addr))
+  buffer = bytearray(1024)  # Create a mutable buffer
+  bytes_received, address = conn.recvfrom_into(buffer)  # Receive data into the buffer and get the sender's address
+  print("Received from:", address)
+  print("Received data:", buffer[:bytes_received])
+  conn.send('HTTP/1.1 200 OK\r\n')
+  conn.send('Content-Type: text/html\r\n')
+  conn.send(f'Access-Control-Allow-Origin: {URL_other}\r\n')
+  conn.send('Access-Control-Allow-Credentials: true\r\n')
+  conn.send('\r\n')
+  conn.send(response)
+  conn.close()
